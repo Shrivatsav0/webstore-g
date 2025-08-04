98bdca20459675975cc3b3a10fea062136378aa6
Millosaurs <millosaurs@gmail.com>
2025-08-04 15:47:54 +0000

    better-auth type issue

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,25 +1,19 @@
-
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
 import * as schema from "../db/schema/auth";
 
-export const auth = betterAuth({
+type AuthType = ReturnType<typeof betterAuth>;
+
+export const auth: AuthType = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    
-    
     schema: schema,
   }),
-  trustedOrigins: [
-    process.env.CORS_ORIGIN || "",
-  ],
+  trustedOrigins: [process.env.CORS_ORIGIN || ""],
   emailAndPassword: {
     enabled: true,
   },
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
 });
-
-
-
